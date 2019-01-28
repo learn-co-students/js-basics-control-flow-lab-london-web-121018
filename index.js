9e55cc146655f6b4545650fb247b6b97d29ef6d2
github username <github email address>
2019-01-28 08:54:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance){
+  let price;
+  if (distance <= 400){
+    price = "This one is on me!"
+  } else if (distance <= 2500 && distance > 2000){
+    price = 'I will gladly take your thirty bucks.'
+  } else if (distance > 2500){
+    price = 'No can do.'
+  }
+  return price;
+}
+
+function ternaryCheckCity(destination){
+  let response
+  destination === 'NYC' ?  response = "Ok, sounds good." : response = "No go.";
+  return response
+}
+
+function switchOnCharmFromTip(tip){
+  let response
+  switch (tip){
+    case 'generous':
+      response = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      response = 'Thank you.'
+      break;
+    default:
+      response = 'Bye.'
+  }
+  return response
+}
